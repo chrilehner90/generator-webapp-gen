6eceda0963b15db95ee7b60ed115c32c2457bd5a
Christian Lehner <lehner.chri@gmail.com>
2014-03-19 14:28:09 +0100

    added package checkbox

--- app/index.js
+++ app/index.js
@@ -26,14 +26,44 @@ var WebappGenGenerator = yeoman.generators.Base.extend({
     this.log(chalk.magenta('You\'re using the fantastic WebappGen generator.'));
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      type: 'input',
+      name: 'appTitle',
+      message: 'What\'s the name of your new fancy app?',
+      default: 'New Awesome App'
+    },{
+      type: 'checkbox',
+      name: 'bowerPackages',
+      message: 'Which bower packages do you want to install, sir?',
+      choices: [
+        {
+          name: 'Bootstrap',
+          value: 'includeBootstrap',
+          checked: true
+        }, {
+          name: 'jQuery',
+          value: 'includeJQuery',
+          checked: true
+        }, {
+          name: 'Angular JS',
+          value: 'includeAngular',
+          checked: false
+        }
+      ]
     }];
 
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+    this.prompt(prompts, function (features) {
+      function includedBowerFeature(f) { return bower.indexOf(f) !== -1; }
+
+
+      var bower = features.bowerPackages
+
+      this.appTitle = features.appTitle;
+
+      console.log(this.title);
+
+      this.bootstrap = includedBowerFeature("includeBootstrap");
+      this.jQuery = includedBowerFeature("includeJQuery");
+      this.angular = includedBowerFeature("includeAngular");
 
       done();
     }.bind(this));
@@ -42,6 +72,11 @@ var WebappGenGenerator = yeoman.generators.Base.extend({
   app: function () {
     this.mkdir('app');
     this.mkdir('app/templates');
+    this.mkdir('app/assets');
+    this.mkdir('app/assets/images');
+    this.mkdir('app/assets/stylesheets');
+    this.mkdir('app/assets/javascripts');
+    this.mkdir('app/assets/fonts');
 
     this.copy('_package.json', 'package.json');
     this.copy('_bower.json', 'bower.json');
